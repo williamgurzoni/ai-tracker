122071829fa27311ec0a5ec78f897d23a5c3d2ef
williamgurzoni <william_luizat@hotmail.com>
2024-09-18 22:09:45 +1200

    Add PWA capabilities for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { InstallPrompt } from './components/PWA';
 import Title from './components/title';
 import Repos from './repos';
 import Stats from './stats';
@@ -5,6 +6,7 @@ import Stats from './stats';
 export default function Home() {
   return (
     <div>
+      <InstallPrompt />
       <div className="h-screen flex justify-center items-center">
         <div className="flex flex-col gap-10">
           <Title>AI Tracker for Devs</Title>
